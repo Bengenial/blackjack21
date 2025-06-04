7ae14a7632b3d31b92f2a658de50cb825c2ca223
Anselmo-kw <anselmo.diaz.v@mail.cl>
2025-06-04 09:10:28 -0400

    Posible estructura

--- blackjack21_prueba.c
+++ blackjack21_prueba.c
@@ -1,9 +1,21 @@
 #include <stdlib.h>
+//#include <list.h> //?
 #include <stdio.h>
+
+typedef struct blackjack21_prueba
+{
+    //aqui ver que vamos a poner, posibleemnte una lista de las cartas?
+    //list* cartas;
+    //int sumaTotal;
+}Jugador;
+
+
+
 
 int main(){
 
     //benja esto es una prueba omg perdi
     //es necesario hacer siempre estos comits?
+    //al parecer si
     return 0;
 }
